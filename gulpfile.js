4e69ba34eaaf48936172a1478636a9b7e0875771
Wang Jing <wangjild@gmail.com>
2017-08-25 21:05:11 +0800

    rename css
    Change-Id: I24b1a019be640f6c987867ce68933d0198872d3c

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ var concatCss = require('gulp-concat-css');
 var plumber = require('gulp-plumber');
 
 gulp.task('concat-css', function () {
-  gulp.src(['./cf-icon.css'])
+  gulp.src(['./cf-icons.css'])
   .pipe(plumber())
-  .pipe(concatCss('./cf-icon.min.css'))
+  .pipe(concatCss('./cf-icons.min.css'))
   .pipe(gulp.dest('./'));
 });
 
 gulp.task('minify-css', function() {
-  gulp.src('./cf-icon.min.css')
+  gulp.src('./cf-icons.min.css')
   .pipe(plumber())
   .pipe(minifyCSS())
   .pipe(gulp.dest('./'))
